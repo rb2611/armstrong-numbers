fe3f4104a1844186db354de51d5b0e4bb3b7ede7
Ramanand Bhagat <ramanandbhagat79@gmail.com>
2020-07-10 10:09:37 +0530

    memory usage

--- index.js
+++ index.js
@@ -1,83 +1,67 @@
 function Armstrong()
 {
   var start = window.performance.now();
-  var flag,number,remainder,addition = 0;
-  var memory = 4;
-  number = Number(document.getElementById("N").value);
-  flag = number;
-  memory+=24;
+  var script=document.createElement('script');
+  var flag,noo,higher;
+  noo = Number(document.getElementById("N").value);
+  flag = noo;
+  lower = noo;
+  higher = noo;
+  var aa = arm(noo);
+  if(aa == flag)
+  {
+    document.getElementById("result").innerHTML=flag + "  is an Armstrong Number.";
+    var end = window.performance.now();
+    var tt=end-start;
+    document.getElementById("time").innerHTML="Execution Time  :  "+ tt.toFixed(5)+ "  ms";
+    document.getElementById("memory").innerHTML="Memmory Usage   :  "+ mem + "  bytes";
+  }
+  else
+    {
+      lower-=1;
+      //for lower
+      while(lower>0)
+      {
+        var low = arm(lower);
+        if(low == lower)
+        {
+          document.getElementById("result").innerHTML="The closest lower Armstrong number is  :  "+lower;
+          break;
+        }
+        lower-=1;
+      }
+      higher+=1;
+      //for lower
+      while(higher>0)
+      {
+        var high = arm(higher);
+        if(high == higher)
+        {
+          document.getElementById("r").innerHTML="The closest higher Armstrong number is  :  "+higher;
+          var end = window.performance.now();
+          var tt=end-start;
+          document.getElementById("time").innerHTML="Execution Time  :  "+ tt.toFixed(5)+ "  ms";
+          break;
+        }
+        higher+=1;
+      }
+    }
+
+}
+function arm(n)
+{
+  this.number=n;
   var l=number.toString().length;
-  var lower=number,higher=number;
-  memory+=12;
+  var remainder,addition=0;
   while(number > 0)
   {
     remainder = number%10;
     addition = addition + remainder**l;
     number = parseInt(number/10);
   }
-
-  if(addition == flag)
-  {
-    document.getElementById("result").innerHTML=flag + "  is an Armstrong Number.";
-    var end = window.performance.now();
-    var tt=end-start;
-    memory+=16;
-    document.getElementById("time").innerHTML="Execution Time  :  "+ tt.toFixed(5)+ "  ms";
-    //document.getElementById("memory").innerHTML="Memmory Usage   :  "+ memory + "  bytes";
-    }
-  else
-  {
-    //low();
-    while(lower>0)
-    {
-      var n=lower,rem,add=0;
-      var len=lower.toString().length;
-      memory+=16;
-      while(n > 0)
-      {
-        rem = n%10;
-        add = add + rem**len;
-        n = parseInt(n/10);
-      }
-
-      if(add == lower)
-      {
-        document.getElementById("result").innerHTML="The closest lower Armstrong number is  :  "+lower;
-        break;
-      }
-      else {
-        lower=lower-1;
-      }
-
-    }
-    //high(higher);
-    while(higher>0)
-    {
-      var h=higher,r,sum=0;
-      var len=higher.toString().length;
-      memory+=16;
-      while(h > 0)
-      {
-        r = h%10;
-        sum = sum + r**len;
-        h = parseInt(h/10);
-      }
-
-      if(sum == higher)
-      {
-        document.getElementById("r").innerHTML="The closest higher Armstrong number is  :  "+higher;
-        var end = window.performance.now();
-        var t = end-start;
-        memory+=16;
-        document.getElementById("time").innerHTML="Execution Time  : "+ t.toFixed(5) + "  ms";
-        //document.getElementById("memory").innerHTML="Memmory Usage   :  "+memory+ "  bytes";
-        break;
-      }
-      else {
-        higher=higher+1;
-      }
-
-    }
-
+  return addition;
 }
+const used = process.memoryUsage();
+for (let key in used) {
+  console.log(`Memory: ${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
 }
